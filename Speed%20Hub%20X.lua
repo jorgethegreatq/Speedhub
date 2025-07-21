673f8c9bcf93e391012a4e4954ee95e0bb3a7930
jorgethegreatq <annzkecastillo456@gmail.com>
2025-07-21 16:57:04 +0800

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/70KpkwsD/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/DBiceC6nMa/0", true))()
